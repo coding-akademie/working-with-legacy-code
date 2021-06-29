3fe127e1c9adaea49a0712e36921ce040a12331d
Michael Mai <michael.mai@agilesoftwaredesign.de>
2021-06-29 16:23:35 +0200

    Fixing test class name

--- excercise/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ excercise/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -1,10 +1,10 @@
 package org.craftedsw.tripservicekata.trip;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TripServiceTest {
+import org.junit.jupiter.api.Test;
+
+public class TripServiceShould {
     @Test
     public void you_should_code_your_tests_here() {
         assertEquals(1, 0);
