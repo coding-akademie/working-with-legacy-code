d0477505285cefaa1da57e8c54922a3e3b785421
Michael Mai <michael.mai@agilesoftwaredesign.de>
2021-06-29 16:50:34 +0200

    Added throw_an_exception_if_user_is_not_logged_in

--- excercise/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ excercise/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -1,12 +1,29 @@
 package org.craftedsw.tripservicekata.trip;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
 import org.junit.jupiter.api.Test;
 
 public class TripServiceShould {
+    private static final User GUEST = null;
+    private static final User UNUSED_USER = null;
+    private User loggedInUser = null;
+
     @Test
-    public void you_should_code_your_tests_here() {
-        assertEquals(1, 0);
+    void throw_an_exception_if_user_is_not_logged_in() {
+        TripService tripService = new TestableTripService();
+        loggedInUser = GUEST;
+
+        assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(UNUSED_USER));
+    }
+
+    private class TestableTripService extends TripService {
+
+        @Override
+        protected User getLoggedInUser() {
+            return loggedInUser;
+        }
     }
 }
